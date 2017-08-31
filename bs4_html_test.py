c39b5d1f184623b72a99b46a60ef84d9ae917e7a
smallbearpapa <bear198512>
2017-08-31 19:03:58 +0800

    html

---
+++ bs4_html_test.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+import requests
+
+r=requests.get("http://python123.io/ws/demo.html")
+demo=r.text
+soup=BeautifulSoup(demo,"html.parser")
+#print(soup.head)
+#print(soup.head.contents)
+#print(soup.body.contents)
+#print(len(soup.body.contents))
+'''print(soup.body.contents[1])
+for item in soup.body.children:
+    print(item)
+
+for item in soup.body.descendants:
+    print(item)
+    '''
+for parent in soup.a.parents:
+    if parent is None:
+        print(parent)
+    else:
+        print(parent.name)
